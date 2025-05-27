4a2c92b015ae16abb5676e4ec03f18593da3e534
DESKTOP-RU8OIBC\srushti.lokhande <Srushti.Lokhande@pivotree.com>
2025-05-27 19:03:04 +0530

    DAY6 commit

--- DAY6/py_s6a1_palindrome.py
+++ DAY6/py_s6a1_palindrome.py
@@ -5,16 +5,23 @@
 # Accepts a word (or sentence) from the player.
 # Checks if it is a palindrome using string manipulation.
 
+
 print("--Palindrome using string manipulation--")
 word = input("Enter a word or sentence: ")
-i = 0
-j = len(word) -1
+word = word.replace(" ", "")  # Removes spaces
 
-while i<j:
+print(word)
+i = 0
+j = len(word) - 1
+is_palindrome = True
+
+while i <= j:
+    print(word[i])
+    print(word[j])
+    print("-----------------------")
     if word[i] != word[j]:
         is_palindrome = False
-    else:
-        is_palindrome = True
+        break
     i += 1
     j -= 1
 
